3c3ea549bc13d6c6d9ec087d8dbeaa03d4e186bd
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2022-07-20 21:54:23 +0300

    add tailwind and prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
@@ -51,6 +51,9 @@ const Home: NextPage = () => {
             </p>
           </a>
         </div>
+        <h1 className="text-3xl font-bold underline text-red-300">
+          Tailwind test
+        </h1>
       </main>
 
       <footer className={styles.footer}>
@@ -66,7 +69,7 @@ const Home: NextPage = () => {
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
