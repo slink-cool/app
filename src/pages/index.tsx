43dd1d1fe7ab7d0bec858f86fa4d471ca0577338
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2022-07-22 00:44:12 +0300

    page layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,29 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import { WalletButton } from '@entities/wallet';
+import { useWallet } from '@solana/wallet-adapter-react';
 import { Sidebar } from '@widgets/Sidebar';
+import type { NextPage } from 'next';
+import { useCallback } from 'react';
 
 const Home: NextPage = () => {
+  const wallet = useWallet();
+
   return (
-    <div className="container">
+    <div className="flex flex-row">
       <Sidebar />
+      <div className="container px-24">
+        <main className="grid-cols-8">
+          <div className="mt-8 h-[348px] col-span-8 bg-primary rounded-xl overflow-hidden mb-10">
+            <div className="h-1/2 bg-[#D3EDFF]" />
+            <div className="px-6">
+              <div className="bg-[#D3EDFF] rounded-full w-32 aspect-square border-4 border-[#201D24] -mt-16 flex justify-center items-center mb-2">
+                <span className='text-6xl text-[#819CAF]'>{wallet.publicKey?.toString()[0]}</span>
+              </div>
+              <span>{wallet.publicKey?.toString()}</span>
+            </div>
+          </div>
+          <div className='grid-cols-8 bg-primary h-[200px] rounded-xl overflow-hidden'>
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
