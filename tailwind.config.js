69093b12ecbfb681ffbf6a79ae61008032b2aa2a
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2022-07-22 14:24:13 +0300

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        light: {
+          500: '#FFFFFF',
+          400: '#958E9E',
+          300: '#605A69',
+        },
+        dark: {
+          500: '#16131A',
+          400: '#201D24',
+          300: '#2C2830',
+        },
+      },
       backgroundColor: {
         primary: '#201D24',
         secondary: '#2C2830',
