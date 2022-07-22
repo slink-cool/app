29443bec0cfa2aa4bc49fd560adfbebefaa22832
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2022-07-22 14:16:25 +0300

    add for u section

--- src/widgets/Sidebar/ui/index.tsx
+++ src/widgets/Sidebar/ui/index.tsx
@@ -4,6 +4,8 @@ import UserGroupOutline from '@shared/icons/UserGroupOutline.svg';
 import ViewGridOutline from '@shared/icons/ViewGridOutline.svg';
 import ExternalLinkOutline from '@shared/icons/ExternalLinkOutline.svg';
 import Logo from '@shared/icons/Logo.svg';
+import NewspaperOutline from '@shared/icons/NewspaperOutline.svg';
+import PlusCircleOutline from '@shared/icons/PlusCircleOutline.svg';
 
 const discover = [
   { Icon: ViewGridOutline, title: 'DAOs' },
@@ -21,24 +23,46 @@ const discover = [
   },
 ];
 
+const forYou = [
+  {
+    Icon: NewspaperOutline,
+    title: 'My Bounties',
+  },
+  {
+    Icon: PlusCircleOutline,
+    title: 'Add your DAO',
+  },
+];
+
 const Sidebar = () => {
   return (
     <div className="w-80 bg-primary h-screen">
       <div className="flex flex-col py-8 px-6">
         <div className="flex flex-row items-center mb-8">
-          <Logo className='mr-2' />
-          <span className='text-2xl font-bold'>delink</span>
+          <Logo className="mr-2" />
+          <span className="text-2xl font-bold">delink</span>
         </div>
         <div className="mb-12">
           <WalletButton />
         </div>
         <div className="flex flex-col">
-          <span className="text-secondary mb-4 uppercase">Discover</span>
+          <span className="text-secondary mb-4 uppercase font-bold">Discover</span>
           <div className="flex flex-col px-5 text-primary">
             {discover.map(({ title, Icon }) => (
               <div key={title} className="flex flex-row mb-6">
                 <Icon className="mr-3" />
-                <span>{title}</span>
+                <span className="font-bold">{title}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <span className="text-secondary mb-4 uppercase font-bold">for you</span>
+          <div className="flex flex-col px-5 text-primary">
+            {forYou.map(({ title, Icon }) => (
+              <div key={title} className="flex flex-row mb-6">
+                <Icon className="mr-3" />
+                <span className="font-bold">{title}</span>
               </div>
             ))}
           </div>
