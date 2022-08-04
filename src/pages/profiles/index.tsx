9bf235c499969b5d61183c410094c381bb40f32e
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2022-08-04 03:09:22 +0300

    working search in profile

--- src/pages/profiles/index.tsx
+++ src/pages/profiles/index.tsx
@@ -1,7 +1,45 @@
+import { PageHeader } from '@shared/ui';
+import { PublicKey } from '@solana/web3.js';
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 
 const ProfilesPage: NextPage = () => {
-  return null;
+  const router = useRouter();
+
+  const isLoading = true;
+
+  return (
+    <>
+      <div className="container sticky top-0 mb-4 border-b border-dark-300 bg-dark-500 px-24  py-4">
+        <PageHeader
+          title="Profile"
+          goBack={router.back}
+          goToAvailable
+          onGoToClick={(value) =>
+            router.push({ pathname: 'profiles/[id]', query: { id: value } })
+          }
+          goToValidator={(value) => {
+            try {
+              const key = new PublicKey(value);
+              console.log(key);
+              return Boolean(key);
+            } catch {
+              return false;
+            }
+          }}
+        />
+      </div>
+      <div className="container grid grid-cols-8 gap-6 px-24">
+        {isLoading &&
+          [...Array(6)].map((it) => (
+            <div key={it} className="col-span-8 overflow-hidden rounded-xl">
+              <div className="h-[120px] bg-primary" />
+            </div>
+          ))}
+        {/* insert data */}
+      </div>
+    </>
+  );
 };
 
 export default ProfilesPage;
--- src/shared/ui/PageHeader.tsx
+++ src/shared/ui/PageHeader.tsx
@@ -1,13 +1,66 @@
+import { Combobox } from '@headlessui/react';
+import React, { useState } from 'react';
+import SearchIcon from '../icons/Search.svg';
 import ArrowLeft from '../icons/ArrowLeft.svg';
-import Search from '../icons/Search.svg';
-import TextInput from './TextInput';
+import clsx from 'clsx';
 
-interface PageHeaderProps {
+interface SearchProps {
+  goToAvailable?: boolean;
+  onGoToClick?: (query: string) => void;
+  goToValidator?: (query: string) => boolean;
+}
+
+const Search: React.FC<SearchProps> = ({
+  goToAvailable,
+  onGoToClick,
+  goToValidator,
+}) => {
+  const [query, setQuery] = useState('');
+
+  const queryValidForGoto = goToValidator ? goToValidator?.(query) : true;
+
+  return (
+    <Combobox value={query} onChange={setQuery}>
+      {({ open }) => (
+        <div className="relative">
+          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 text-light-400">
+            <SearchIcon />
+          </div>
+          <Combobox.Input
+            className={clsx(
+              'w-full rounded border-transparent bg-dark-300 p-3 pl-9 text-label text-white placeholder:text-light-400 focus:border-dark-200 focus:ring-0',
+              open && 'rounded-b-none border-b-0'
+            )}
+            placeholder="Search"
+            onChange={(event) => setQuery(event.target.value)}
+          />
+
+          <Combobox.Options className="absolute z-10 w-full truncate rounded-b border-x border-b border-dark-200 bg-dark-300 p-3">
+            {goToAvailable && query.length > 0 && queryValidForGoto && (
+              <Combobox.Option
+                value={{ id: null, name: query }}
+                onClick={() => onGoToClick?.(query)}
+              >
+                Go to {query}
+              </Combobox.Option>
+            )}
+          </Combobox.Options>
+        </div>
+      )}
+    </Combobox>
+  );
+};
+
+interface PageHeaderProps extends SearchProps {
   title: string;
   goBack?: () => void;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title, goBack }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({
+  title,
+  goBack,
+  ...searchProps
+}) => {
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center">
@@ -17,12 +70,7 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title, goBack }) => {
         <h1 className="text-title-h1">{title}</h1>
       </div>
       <div className="w-56">
-        <TextInput
-          id="search"
-          placeholder="Search"
-          leftAdornment={<Search />}
-          className="pl-9"
-        />
+        <Search {...searchProps} />
       </div>
     </div>
   );
