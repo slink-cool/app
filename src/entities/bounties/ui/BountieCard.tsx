a945d278227aa83e997fd1eb5e8c463be94e1533
Evgeny Galtsov <uxpkrvs@gmail.com>
2022-08-04 17:02:45 +0300

    test cards for example

--- src/entities/bounties/ui/BountieCard.tsx
+++ src/entities/bounties/ui/BountieCard.tsx
@@ -28,36 +28,38 @@ const BountieCard: React.FC<BountieCardProps> = ({
 }) => {
   return (
     <Link href={sourceUrl} passHref>
-      <div className="flex h-[21rem] cursor-pointer flex-col rounded-xl bg-dark-400 p-6 transition hover:ring-2 hover:ring-dark-300">
-        <div className="flex items-center">
-          <div className="aspect-square w-[32px] overflow-hidden rounded-full">
-            {avatarUrl ? (
-              <object data={avatarUrl} type="image/jpeg" className="w-[32px]">
-                <LogoFallback title={bountieTitle} />
-              </object>
-            ) : (
-              <LogoFallback title={bountieTitle} />
-            )}
+      <a target="_blank">
+        <div className="flex h-[20rem] cursor-pointer flex-col rounded-xl bg-dark-400 p-6 transition hover:ring-2 hover:ring-dark-300">
+          <div className="flex items-center">
+            <div className="aspect-square w-[32px] overflow-hidden rounded-full">
+              {avatarUrl ? (
+                <object data={avatarUrl} type="image/jpeg" className="w-[32px]">
+                  <LogoFallback title={daoTitle} />
+                </object>
+              ) : (
+                <LogoFallback title={daoTitle} />
+              )}
+            </div>
+            <div className="ml-3 flex flex-col">
+              <span className="text-label text-light-400">{daoTitle}</span>
+              <span className="mt-1 text-caption text-light-300">
+                {daoPublished}
+              </span>
+            </div>
           </div>
-          <div className="ml-3 flex flex-col">
-            <span className="text-label text-light-400">{daoTitle}</span>
-            <span className="mt-1 text-caption text-light-300">
-              {daoPublished}
-            </span>
+          <div className="mt-12 flex h-full flex-col justify-between">
+            <div className="flex flex-col">
+              <span className="text-caption text-light-300">{bountieTag}</span>
+              <span className="mt-2 text-subtitle-h1 text-light-500">
+                {bountieTitle}
+              </span>
+            </div>
+            <div className="w-fit rounded bg-accent-500 px-2 py-1">
+              <span className="text-label text-light-500">{bountieReward}</span>
+            </div>
           </div>
         </div>
-        <div className="mt-12 flex h-full flex-col justify-between">
-          <div className="flex flex-col">
-            <span className="text-caption text-light-300">{bountieTag}</span>
-            <span className="mt-2 text-subtitle-h1 text-light-500">
-              {bountieTitle}
-            </span>
-          </div>
-          <div className="w-fit rounded bg-accent-500 px-2 py-1">
-            <span className="text-label text-light-500">{bountieReward}</span>
-          </div>
-        </div>
-      </div>
+      </a>
     </Link>
   );
 };
