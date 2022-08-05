bcd10dbfbea29521a6f4e6a99493c85ab741e885
Evgeny Galtsov <uxpkrvs@gmail.com>
2022-08-05 09:37:11 +0300

    feat: bounties page layout (#4)
    * bounties page layout
    
    * test cards for example
    
    * logoFallback font size fix

---
+++ src/entities/bounties/ui/BountieCard.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import Link from 'next/link';
+
+interface BountieCardProps {
+  avatarUrl?: string;
+  daoTitle: string;
+  daoPublished?: string;
+  bountieTag?: string;
+  bountieTitle: string;
+  bountieReward: string;
+  sourceUrl: string;
+}
+
+const LogoFallback: React.FC<{ title: string }> = ({ title }) => (
+  <div className="flex aspect-square items-center justify-center bg-[#D3EDFF]">
+    <span className="text-2xl text-[#819CAF]">{title[0]}</span>
+  </div>
+);
+
+const BountieCard: React.FC<BountieCardProps> = ({
+  daoTitle,
+  daoPublished,
+  bountieTag,
+  bountieTitle,
+  bountieReward,
+  avatarUrl,
+  sourceUrl,
+}) => {
+  return (
+    <Link href={sourceUrl} passHref>
+      <a target="_blank">
+        <div className="flex h-[20rem] cursor-pointer flex-col rounded-xl bg-dark-400 p-6 transition hover:ring-2 hover:ring-dark-300">
+          <div className="flex items-center">
+            <div className="aspect-square w-[32px] overflow-hidden rounded-full">
+              {avatarUrl ? (
+                <object data={avatarUrl} type="image/jpeg" className="w-[32px]">
+                  <LogoFallback title={daoTitle} />
+                </object>
+              ) : (
+                <LogoFallback title={daoTitle} />
+              )}
+            </div>
+            <div className="ml-3 flex flex-col">
+              <span className="text-label text-light-400">{daoTitle}</span>
+              <span className="mt-1 text-caption text-light-300">
+                {daoPublished}
+              </span>
+            </div>
+          </div>
+          <div className="mt-12 flex h-full flex-col justify-between">
+            <div className="flex flex-col">
+              <span className="text-caption text-light-300">{bountieTag}</span>
+              <span className="mt-2 text-subtitle-h1 text-light-500">
+                {bountieTitle}
+              </span>
+            </div>
+            <div className="w-fit rounded bg-accent-500 px-2 py-1">
+              <span className="text-label text-light-500">{bountieReward}</span>
+            </div>
+          </div>
+        </div>
+      </a>
+    </Link>
+  );
+};
+
+export default BountieCard;
---
+++ src/pages/bounties/index.tsx
@@ -0,0 +1,80 @@
+import { BountieCard } from '@entities/bounties';
+import { PageHeader } from '@shared/ui';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+
+const BountiesPage: NextPage = () => {
+  const router = useRouter();
+
+  return (
+    <>
+      <div className="container sticky top-0 mb-4 border-b border-dark-300 bg-dark-500 px-24  py-4">
+        <PageHeader title="Bounties" goBack={router.back} />
+      </div>
+      <div className="container grid grid-cols-8 gap-6 px-24">
+        <div className="col-span-2">
+          <BountieCard
+            avatarUrl={
+              'https://stakingcrypto.info/static/assets/coins/solana-logo.png?v=34'
+            }
+            daoTitle={'Solana'}
+            daoPublished={'May 31, 2022'}
+            bountieTag={'Development'}
+            bountieTitle={
+              'Build a Solana + Filecoin/IPFS Project for the Bangalore Hacker House'
+            }
+            bountieReward={'$3000 USDC'}
+            sourceUrl={
+              'https://superteam.fun/bounties/build-a-solana-filecoinipfs-project-for-the-bangalore-hacker-house'
+            }
+          />
+        </div>
+        <div className="col-span-2">
+          <BountieCard
+            avatarUrl={
+              'https://pbs.twimg.com/profile_images/1493136166975160328/S_HMX8bX_400x400.jpg'
+            }
+            daoTitle={'Crowdpad'}
+            daoPublished={'May 28, 2022'}
+            bountieTag={'Research'}
+            bountieTitle={'Crowdpad Deep Dive'}
+            bountieReward={'$1000 USDC'}
+            sourceUrl={'https://superteam.fun/bounties/crowdpad-deep-dive'}
+          />
+        </div>
+        <div className="col-span-2">
+          <BountieCard
+            avatarUrl={
+              'https://blog.coindcx.com/wp-content/uploads/2021/09/ProfilePic.svg'
+            }
+            daoTitle={'CoinDCX'}
+            daoPublished={'May 1, 2022'}
+            bountieTag={'Research'}
+            bountieTitle={'Evaluating Web3 Projects Deep Dive'}
+            bountieReward={'$700 USDC'}
+            sourceUrl={
+              'https://superteam.fun/bounties/coindcx-deep-dive-bounty'
+            }
+          />
+        </div>
+        <div className="col-span-2">
+          <BountieCard
+            avatarUrl={
+              'https://bucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com/public/images/5bf07e40-81d8-42ec-a060-5a9863ffe002_424x424.jpeg'
+            }
+            daoTitle={'Hyperspace'}
+            daoPublished={'April 09, 2022'}
+            bountieTag={'Design'}
+            bountieTitle={'Hyperspace Logo Design Bounty'}
+            bountieReward={'$1000 USDC'}
+            sourceUrl={
+              'https://superteam.fun/bounties/hyperspace-logo-design-bounty'
+            }
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BountiesPage;
