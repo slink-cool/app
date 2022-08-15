731f7af0017817ebd8bf4a625d3c1f28400ea3ac
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2022-08-15 13:22:20 +0300

    fix double msg signing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,7 @@ import { Sidebar } from '@widgets/Sidebar';
 import { decodeJwt } from 'jose';
 import type { AppProps } from 'next/app';
 import { parseCookies } from 'nookies';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 
@@ -23,28 +23,41 @@ dayjs.extend(utc);
 function App({ Component, pageProps }: AppProps) {
   const wallet = useWallet();
 
+  const [isWalletSigning, setIsWalletSigning] = useState(false);
+
   useEffect(() => {
-    if (!wallet.connected || !wallet.publicKey || !wallet.signMessage) return;
+    if (
+      !wallet.connected ||
+      !wallet.publicKey ||
+      !wallet.signMessage ||
+      isWalletSigning
+    )
+      return;
 
-    const cookies = parseCookies();
-    const accessToken = cookies['sb-access-token'];
-    if (accessToken) {
-      const jwtTokenParsed = decodeJwt(accessToken);
-      if (
-        jwtTokenParsed &&
-        jwtTokenParsed.sub === wallet.publicKey.toBase58()
-      ) {
-        supabase.auth.setAuth(accessToken);
-        return;
+    setIsWalletSigning(true);
+    try {
+      const cookies = parseCookies();
+      const accessToken = cookies['sb-access-token'];
+      if (accessToken) {
+        const jwtTokenParsed = decodeJwt(accessToken);
+        if (
+          jwtTokenParsed &&
+          jwtTokenParsed.sub === wallet.publicKey.toBase58()
+        ) {
+          supabase.auth.setAuth(accessToken);
+          return;
+        }
       }
-    }
 
-    WalletToken.new(wallet.publicKey, wallet.signMessage).then((jwt) => {
-      requestAccessToken(jwt.toString()).then(({ token }) =>
-        supabase.auth.setAuth(token)
-      );
-    });
-  }, [wallet]);
+      WalletToken.new(wallet.publicKey, wallet.signMessage).then((jwt) => {
+        requestAccessToken(jwt.toString()).then(({ token }) =>
+          supabase.auth.setAuth(token)
+        );
+      });
+    } finally {
+      setIsWalletSigning(false);
+    }
+  }, [wallet, isWalletSigning]);
 
   return (
     <div className="flex flex-row bg-dark-500 text-white">
